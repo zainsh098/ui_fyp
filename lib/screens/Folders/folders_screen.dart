bed547e4ba72ab11d0508bfaa694a07b01f1e288
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-10-17 09:28:13 +0500

    Work start on Folder Screen

--- lib/screens/Folders/folders_screen.dart
+++ lib/screens/Folders/folders_screen.dart
@@ -4,47 +4,135 @@ import 'package:ui_fyp/res/font_styles.dart';
 
 import '../../res/components/bottom_navigation_bar.dart';
 
-
 class FoldersMainScreen extends StatelessWidget {
   const FoldersMainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       bottomNavigationBar: BottomNavgationBar(),
       body: SafeArea(
         child: Column(
-
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-              SizedBox(height: 10,),
+            SizedBox(
+              height: 15,
+            ),
             Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset('assets/google.png',height: 15,width: 15,),
-                SizedBox(width: 5,),
-                TextWidget(text: 'Files', textStyle:TextStyle(fontSize: 20, fontWeight: FontWeight.bold,fontFamily: FontStyles.CarosSoftBold),),
-                IconButton(onPressed: () {
-
-                }, icon: Icon(Icons.search,size: 35,)),
-                IconButton(onPressed: () {
-
-                }, icon: Icon(Icons.more_rounded,size: 30,)),
-
-
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/google.png',
+                      height: 20,
+                      width: 20,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    TextWidget(
+                      text: 'Files',
+                      textStyle: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: FontStyles.CarosSoftBold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // Add your onPressed logic here
+                      },
+                      icon: Icon(
+                        Icons.search,
+                        size: 25,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Add your onPressed logic here
+                      },
+                      icon: Icon(
+                        Icons.more_rounded,
+                        size: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                ),
               ],
-
+            ),
+            Divider(
+              endIndent: 15,
+              indent: 10,
 
             ),
 
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
 
+                    Padding(
+                      padding: const EdgeInsets.only(left: 4),
+                      child: TextWidget(
+                        text: 'Total: 128 Files',
+                        textStyle: TextStyle(
+                          fontSize: 16,
+
+                          fontFamily: FontStyles.CarosSoftBold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // Add your onPressed logic here
+                      },
+                      icon: Icon(
+                        Icons.add_road_rounded,
+                        size: 25,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Add your onPressed logic here
+                      },
+                      icon: Icon(
+                        Icons.create_new_folder_outlined,
+                        size: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10
+                    ),
+                  ],
+                ),
+              ],
+            ),
 
           ],
-
-
         ),
       ),
-
     );
   }
 }
